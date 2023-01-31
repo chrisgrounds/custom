d8ab04dca5a20c787df6f3c654ae1f6dcf03107b
chrisgrounds <iamchrisgrounds@gmail.com>
2023-01-31 14:50:31 +0000

    Add rust setup

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,5 +1,11 @@
 local overrides = require "custom.plugins.overrides"
 
+
+local function on_attach(client, buffer)
+  -- This callback is called when the LSP is atttached/enabled for this buffer
+  -- we could set keymaps related to LSP, etc here.
+end
+
 return {
   ["goolord/alpha-nvim"] = { disable = false }, -- enables dashboard
 
@@ -16,10 +22,43 @@ return {
     override_options = overrides.nvimtree,
   },
 
+  ["simrat39/rust-tools.nvim"] = {},
+
   ["neovim/nvim-lspconfig"] = {
     config = function()
       require "plugins.configs.lspconfig"
       require "custom.plugins.lspconfig"
+      require("rust-tools").setup({
+  tools = {
+    runnables = {
+      use_telescope = true,
+    },
+    inlay_hints = {
+      auto = true,
+      show_parameter_hints = false,
+      parameter_hints_prefix = "",
+      other_hints_prefix = "",
+    },
+  },
+
+  -- all the opts to send to nvim-lspconfig
+  -- these override the defaults set by rust-tools.nvim
+  -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
+  server = {
+    -- on_attach is a callback called when the language server attachs to the buffer
+    on_attach = on_attach,
+    settings = {
+      -- to enable rust-analyzer settings visit:
+      -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+      ["rust-analyzer"] = {
+        -- enable clippy on save
+        checkOnSave = {
+          command = "clippy",
+        },
+      },
+    },
+  },
+})
     end,
   },
 
