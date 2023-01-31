a480cb99fff364fe9220a0a7e59dd296cc46da7a
chrisgrounds <iamchrisgrounds@gmail.com>
2023-01-31 15:39:01 +0000

    Add null-ls config

---
+++ plugins/null-ls.lua
@@ -0,0 +1,19 @@
+local present, null_ls = pcall(require, "null-ls");
+
+if not present then
+  return
+end
+
+local b = null_ls.builtins
+
+local sources = {
+
+  b.formatting.rustfmt
+
+}
+
+null_ls.setup {
+  debug = true,
+  sources = sources,
+}
+
