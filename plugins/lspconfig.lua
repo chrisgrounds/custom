392a85fa1261f1afbc9345a93121459c371f095a
chrisgrounds <iamchrisgrounds@gmail.com>
2023-01-31 16:01:22 +0000

    remove rust-analyzer from lspconfig servers, wrong place

--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -3,7 +3,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
-local servers = { "html", "cssls", "tsserver", "clangd", "rust-analyzer" }
+local servers = { "html", "cssls", "tsserver", "clangd" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
